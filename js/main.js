c9dd8de6efa5e99191c6fb6bea561f1b1495a465
Den89 <malyhindenis8@gmail.com>
2022-12-07 18:16:08 +0500

    terw

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(".help__img").click(function(){
+   $(".click").toggleClass('active');
+});
+
+$(".menu__burger").click(function(){
+   $(".menu__burger").toggleClass('active');
+   $(".menu__list").toggleClass('active');
+});
